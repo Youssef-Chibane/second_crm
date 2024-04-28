2d5868537427f8010396f728a95423c3f9bcf176
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-04-28 17:16:59 +0000

    view_leads

--- leads/urls.py
+++ leads/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('leads/', views.leads_list, name='leads_list'),
+    path('leads/', views.leads_list),
+    path('leads/<pk>/', views.lead_detail),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
 def leads_list(request):
     # return HttpResponse("Hello World")
-    return render(request, 'leads_list.html')
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, 'leads_list.html', context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, 'lead_detail.html', context)
