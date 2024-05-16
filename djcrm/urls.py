8d0c262bb8d0adc7fb23a6598683aedb7f4d28f3
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-05-16 23:59:01 +0100

    added_login

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import *
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name="landing-page"),
     path('leads/',  include('leads.urls', namespace="leads")),
+    path('login/', LoginView.as_view(), name="login"),
 ]
 
 if settings.DEBUG:
