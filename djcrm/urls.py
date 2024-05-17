40541b57588c25e4d0dd58769cf5ac19f55dca0a
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-05-17 23:16:09 +0100

    SignupView_LogoutView

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -3,14 +3,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import *
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name="landing-page"),
     path('leads/',  include('leads.urls', namespace="leads")),
+    path('signup/', SignupView.as_view(), name="signup"),
     path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
 
 if settings.DEBUG:
