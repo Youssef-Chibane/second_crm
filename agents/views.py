fccb3920e509103716670766a11a1fe28062f701
impassive <130257842+Youssef-Chibane@users.noreply.github.com>
2024-05-28 22:30:17 +0100

    agent_list_view

--- agents/views.py
+++ agents/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class AgentListView(OrganisorAndLoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+    
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
