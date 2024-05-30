074ed3bd759e58d01362230f05cd857dbfc3d49e
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-05-30 23:11:35 +0100

    clearing_AgentListView

--- agents/views.py
+++ agents/views.py
@@ -1,11 +1,6 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
-from .mixins import OrganisorAndLoginRequiredMixin
 
-class AgentListView(OrganisorAndLoginRequiredMixin, generic.ListView):
-    template_name = "agents/agent_list.html"
-    
-    def get_queryset(self):
-        organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation=organisation)
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    pass
