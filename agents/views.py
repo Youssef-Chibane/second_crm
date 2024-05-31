b478a323fa36918b21447b2a55b3d20e73a1e41b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-05-31 21:16:52 +0100

    fixed_AgentListView

--- agents/views.py
+++ agents/views.py
@@ -1,6 +1,11 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
-    pass
+    template_name = "agents/agent_list.html"
+
+    def get_queryset(self):
+        return Agent.objects.all()
