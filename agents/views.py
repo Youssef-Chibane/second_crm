4153eedddfa92b58cc6de8c2ea5739b5846ba29a
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-06-02 22:56:38 +0100

    fixing_agent_list

--- agents/views.py
+++ agents/views.py
@@ -8,6 +8,7 @@ from .forms import AgentModelForm
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
+    context_object_name = "agents"
 
     def get_queryset(self):
         return Agent.objects.all()
