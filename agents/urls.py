0d285bf43eeaed97843c63150461d024107b1ab6
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-06-07 22:58:40 +0100

    AgentDetailView_url

--- agents/urls.py
+++ agents/urls.py
@@ -6,4 +6,5 @@ app_name = 'agents'
 urlpatterns = [
     path('', AgentListView.as_view(), name='agent-list'),
     path('create', AgentCreateView.as_view(), name='agent-create'),
+    path('<int:pk>/', AgentDetailView.as_view(), name='agent-detail'),
 ]
