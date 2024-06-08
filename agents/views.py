0485cb4dea5aec4649f7dd4c48f29b9f55f511f7
impassive <130257842+Youssef-Chibane@users.noreply.github.com>
2024-06-08 23:05:04 +0100

    Update views.py

--- agents/views.py
+++ agents/views.py
@@ -31,3 +31,9 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
 class AgentDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "agents/agent_detail.html"
     context_object_name = "agent"
+
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
