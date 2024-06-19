a89385c54a8225c5454c89a6e1ad8c82123fb2a4
impassive <130257842+Youssef-Chibane@users.noreply.github.com>
2024-06-19 22:39:05 +0100

    Update settings.py

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -113,8 +113,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = "static_root"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
